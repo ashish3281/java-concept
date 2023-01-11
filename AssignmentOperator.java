7d93e46af8d71cac62c3fe539a6ea1043f963676
Ashish_Goyal <92047366+ashish3281@users.noreply.github.com>
2023-01-11 21:11:08 +0530

    Add files via upload

---
+++ AssignmentOperator.java
@@ -0,0 +1,13 @@
+public class AssignmentOperator {
+    public static void main(String args[]){
+    int x=10;
+    x+=5;
+    System.out.println(x);
+    x-=4;
+    System.out.println(x);
+   int  y=6;
+    y |=3;
+System.out.println(y);
+
+    }
+}
