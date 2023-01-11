732dec18fa5c6a9dd83e57334662c5897d5c2063
Ashish_Goyal <92047366+ashish3281@users.noreply.github.com>
2023-01-11 21:11:35 +0530

    Add files via upload

---
+++ comparisonOperator.java
@@ -0,0 +1,12 @@
+public class comparisonOperator {
+    public static void main(String args[]){
+        int x=5;
+        int y=8;
+        System.out.println(x==y);
+        System.out.println(x!=y);
+        System.out.println(x>y);
+        System.out.println(x<y);
+        System.out.println(x>=y);
+        System.out.println(x<=y);
+    }
+}
