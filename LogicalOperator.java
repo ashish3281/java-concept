4556b5ee958a0cd1e182fef92d85cbdab7fb4920
Ashish_Goyal <92047366+ashish3281@users.noreply.github.com>
2023-01-11 21:12:01 +0530

    Add files via upload

---
+++ LogicalOperator.java
@@ -0,0 +1,9 @@
+public class LogicalOperator {
+    public static void main(String args[]){
+        int x=5;
+        System.out.println(x<5 && x<10);
+        System.out.println(x>5||x<4);
+        System.out.println(!(x<5&&x<10));
+
+    }
+}
