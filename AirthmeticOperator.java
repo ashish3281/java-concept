71917b155959e2b21841480e187194e5eb303ee6
Ashish_Goyal <92047366+ashish3281@users.noreply.github.com>
2023-01-11 21:10:45 +0530

    Add files via upload

---
+++ AirthmeticOperator.java
@@ -0,0 +1,18 @@
+public class AirthmeticOperator {
+    public static void main(String args[]){
+    int a=2;
+    int b=4;
+    int c=a+b;
+    int d=b-a;
+    int x=a*b;
+    int y=b/a;
+    int z=a++;
+    int t=b--;
+    System.out.println(c);        
+    System.out.println(d);    
+    System.out.println(x);    
+    System.out.println(y);
+    System.out.println(z);
+    System.out.println(t);            
+    }
+}
