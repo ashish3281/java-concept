4836ccd976162dcc61910ad85773bdb66d01677c
Ashish_Goyal <92047366+ashish3281@users.noreply.github.com>
2023-01-15 20:49:26 +0530

    Add files via upload

---
+++ BooleanExample.java
@@ -0,0 +1,21 @@
+public class BooleanExample {
+    public static void main(String args[]){
+     boolean isJavaFun =true;
+     boolean isJavaFun1=false;
+     System.out.println(isJavaFun);
+     System.out.println(isJavaFun1);
+     System.out.println(10>9);
+    
+
+     int myage=25;
+     int votingAge=18;
+     if(myage>=votingAge){
+        System.out.println("old enough to vote");
+
+     }
+     else{
+        System.out.println("Not old enough to vote");
+     }
+    }
+
+}
