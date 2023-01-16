99b0b9690fcce105b4ddad151dbeca1aa0ef078c
Ashish_Goyal <92047366+ashish3281@users.noreply.github.com>
2023-01-16 11:30:49 +0530

    Add files via upload

---
+++ MathExample.java
@@ -0,0 +1,10 @@
+public class MathExample {
+    public static void main(String args[]){
+       System.out.println( Math.max(5,19));
+        System.out.println(Math.min(5,19));
+        System.out.println(Math.sqrt(64));
+        System.out.println(Math.abs(-4.7));
+        System.out.println(Math.random());
+
+    }
+}
