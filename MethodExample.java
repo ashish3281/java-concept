9b78204fb1cf9d02871a4a370a0c0ed508808bd2
Ashish_Goyal <92047366+ashish3281@users.noreply.github.com>
2023-01-16 11:31:10 +0530

    Add files via upload

---
+++ MethodExample.java
@@ -0,0 +1,28 @@
+// public class MethodExample{
+//     static void myMethod(){
+//         System.out.println("I just got exuted");
+//     }
+//     public static void main(String args[]){
+//     myMethod();
+//     }
+// }
+
+
+// public class MethodExample{
+//     static void myMethod(String fname){
+//         System.out.println(fname+"I just got exuted");
+//     }
+//     public static void main(String args[]){
+//     myMethod("ashish");
+//     }
+// }
+
+public class MethodExample{
+    static int myMethod(int x){
+        return 5+x;
+        
+    }
+    public static void main(String args[]){
+    System.out.println(myMethod(3));
+    }
+}
