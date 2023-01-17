cb5f753dcf03777d678b49f3fa64b1b667dc9c66
Ashish_Goyal <92047366+ashish3281@users.noreply.github.com>
2023-01-17 21:08:16 +0530

    Add files via upload

---
+++ StringBufferExample.java
@@ -0,0 +1,15 @@
+public class StringBufferExample {
+    public static void main(String args[]){
+        StringBuffer sb=new StringBuffer("Hello");
+        sb.append("Java");
+        System.out.println(sb);
+        sb.insert(1,"Java");
+        System.out.println(sb);
+        sb.replace(1,3,"Java");
+        System.out.println(sb);
+        sb.delete(1,3);
+        System.out.println(sb);
+        sb.reverse();
+        System.out.println(sb);
+    }
+}
