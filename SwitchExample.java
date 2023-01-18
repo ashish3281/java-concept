e1be4f1c15dce3793dc329a27b29470578dcfc71
Ashish_Goyal <92047366+ashish3281@users.noreply.github.com>
2023-01-18 19:53:43 +0530

    Add files via upload

---
+++ SwitchExample.java
@@ -0,0 +1,30 @@
+public class SwitchExample {
+    public static void main(String args[]){
+        int day=4;
+        switch(day){
+            case 1:
+            System.out.println("Monday");
+            break;
+            case 2:
+            System.out.println("Tuesday");
+            break;
+            case 3:
+            System.out.println("Wednesday");
+            break;
+            case 4:
+            System.out.println("Thrusday");
+            break;
+            case 5:
+            System.out.println("Friday");
+            break;
+            case 6:
+            System.out.println("Saturday");
+            break;
+            case 7:
+            System.out.println("Sunday");
+            break;
+            default:
+            System.out.println("looking forward to the weekend");
+        }
+    }
+}
