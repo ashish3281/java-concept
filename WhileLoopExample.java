46286e6d6801983c604052007f8e74cd7352c685
Ashish_Goyal <92047366+ashish3281@users.noreply.github.com>
2023-01-18 19:54:54 +0530

    Add files via upload

---
+++ WhileLoopExample.java
@@ -0,0 +1,16 @@
+public class WhileLoopExample {
+    public static void main(String args[]){
+        int i=0;
+        while(i<5){
+            System.out.println(i);
+            i++;
+        }
+
+        int x=0;
+        do{
+            System.out.println(x);
+            x++;
+        }
+        while(x<5);
+    }
+}
