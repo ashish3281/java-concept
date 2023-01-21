a274cd52d6a44bcf59b212a3b7eb17830fd5751e
Ashish_Goyal <92047366+ashish3281@users.noreply.github.com>
2023-01-21 19:57:31 +0530

    Add files via upload

---
+++ MultiObjectExample.java
@@ -0,0 +1,9 @@
+public class MultiObjectExample {
+    int x=5;
+  public static void main(String args[]) {
+    MultiObjectExample obj=new MultiObjectExample();
+    MultiObjectExample obj1=new MultiObjectExample();
+    System.out.println(obj.x);
+    System.out.println(obj1.x);
+  }
+}
