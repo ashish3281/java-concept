758c0e012898aac377bea7275e4a5173244f537c
Ashish_Goyal <92047366+ashish3281@users.noreply.github.com>
2023-01-21 19:55:53 +0530

    Add files via upload

---
+++ objectExample.java
@@ -0,0 +1,8 @@
+public class objectExample {
+   
+    int x=5;
+    public static void main(String[] args){
+        objectExample obj=new objectExample();
+        System.out.println(obj.x);
+    }
+}
