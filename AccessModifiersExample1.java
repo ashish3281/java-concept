3b02028d36e76f794b4192ae76f44b08a2501548
Ashish_Goyal <92047366+ashish3281@users.noreply.github.com>
2023-01-22 15:26:40 +0530

    Add files via upload

---
+++ AccessModifiersExample1.java
@@ -0,0 +1,9 @@
+public class AccessModifiersExample1{
+    String fname="ashish";
+     int  age=21;
+    public static void main(String args[]){
+        AccessModifiersExample1 obj=new AccessModifiersExample1();
+        System.out.println(obj.fname);
+        System.out.println(obj.age);
+    }
+}
