13d3c54e25f579e3c47133850c19ad1513ae60cc
Ashish_Goyal <92047366+ashish3281@users.noreply.github.com>
2023-01-24 11:10:12 +0530

    Add files via upload

---
+++ enumsExample.java
@@ -0,0 +1,11 @@
+public class enumsExample{
+    enum Level{
+        LOW,
+        MEDIUM,
+        HIGH
+    }
+    public static void main(String[] args){
+        Level obj=Level.MEDIUM;
+        System.out.println(obj);
+    }
+}
