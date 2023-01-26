e3f632b2dfa49391625b65f64891402e298b94ec
Ashish_Goyal <92047366+ashish3281@users.noreply.github.com>
2023-01-26 10:11:01 +0530

    Add files via upload

---
+++ wrapperExample1.java
@@ -0,0 +1,8 @@
+public class wrapperExample1 {
+    public static void main(String args[]){
+        Integer myInt=5;
+      String myString=myInt.toString();
+       System.out.println(myString.length());
+    }
+    
+}
